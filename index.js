fb5a5bc91d49a9987715ae85d6c22db0fe4e88a4
Oussama Masri <oussamamasri16120@gmail.com>
2022-08-16 12:18:18 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(event = "roller-skate"){
+    return `This Saturday, I want to ${event}!`;
+}
+
+function mondayWork(event = "go to the office"){
+    return `This Monday, I will ${event}.`;
+}
+
+
+// function wrapAdjective(special = "*" ){
+//     return `You are ${special} a hard worker ${special}!`
+//  }
+
+//  const encouragingPromptFunction = wrapAdjective("*")
+
+function wrapAdjective(style= "*"){
+    return function(string= "special"){
+      return `You are ${style}${string}${style}!`
+    }
+  }
+ 
